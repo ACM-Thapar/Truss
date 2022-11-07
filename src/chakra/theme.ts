a8a87be2fbd56637fe832bb1ef2d4a89d9964bb7
Tusharbecoding <tusharchopra02122002@gmail.com>
2022-11-07 16:47:14 +0530

    setting up

---
+++ src/chakra/theme.ts
@@ -0,0 +1,27 @@
+import '@fontsource/open-sans/300.css';
+import '@fontsource/open-sans/400.css';
+import '@fontsource/open-sans/700.css';
+import { extendTheme } from "@chakra-ui/react"
+
+// 2. Call `extendTheme` and pass your custom values
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#1a1b1d",
+      200: "#f6f6f6",
+    },
+  },
+  fonts: {
+    body: "Open Sans, sans-serif",
+  },
+  styles: {
+    global: () => ({
+      body: {
+        bg: "#232323",
+      },
+    }),
+  },
+  components: {
+    // Button
+  },
+})
