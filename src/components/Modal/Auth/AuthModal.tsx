343c6a7c36aca557fca3caf3b9e0fa043c84eef7
Tusharbecoding <tusharchopra02122002@gmail.com>
2022-11-09 10:52:49 +0530

    added neomorph to modal with form

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -7,10 +7,11 @@ import {
     ModalFooter,
     ModalBody,
     ModalCloseButton,
+    Flex,
 } from '@chakra-ui/react'
 import { useRecoilState } from 'recoil';
 import { authModalState } from '../../../atoms/authModalAtom';
-
+import AuthInputs from './AuthInputs';
 
 const AuthModal:React.FC = () => {
     const [modalState, setModalState] = useRecoilState(authModalState);
@@ -25,15 +26,23 @@ const AuthModal:React.FC = () => {
         <>
             <Modal isOpen={modalState.open} onClose={handleClose}>
                 <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>
+                <ModalContent bg="#1a1b1d" color="white">
+                    <ModalHeader textAlign="center">
                         {modalState.view === 'login' && 'Log In'}
                         {modalState.view === 'signup' && 'Sign Up'}
                         {modalState.view === 'resetPassword' && 'Reset Password'}
                     </ModalHeader>
                     <ModalCloseButton />
-                    <ModalBody display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-
+                    <ModalBody display="flex" flexDirection="column" justifyContent="center" alignItems="center"
+                    pb={6}>
+                        <Flex 
+                        direction="column" 
+                        align="center" 
+                        justify="center" 
+                        width="70%"
+                        >
+                        <AuthInputs />
+                        </Flex>
                     </ModalBody>
                 </ModalContent>
           </Modal>
--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -21,10 +21,10 @@ const SearchInput:React.FC<SearchInputProps> = () => {
                 bg="#1a1b1d" 
                 color="#5596E6"
                 outline="none" border="none"  
-                boxShadow="12px 12px 12px 0 rgba(0,0,0,.2), -12px -12px 12px 0 rgba(58, 58, 58, 0.3)" 
+                boxShadow="inset 5px 5px 7px #141416, inset -5px -5px 7px #212224" 
                 transition="color 0.2s ease-out, transform 0.2s ease-out" 
                 cursor="pointer" 
-                _hover={{boxShadow: "-12px -12px 16px 0 rgba(58, 58, 58, 0.3),12px 12px 16px 0 rgba(0,0,0,.2)"}} 
+                _hover={{boxShadow: "inset 5px 5px 0px #17181a,inset -5px -5px 0px #1d1e20"}} 
                 _focus={{boxShadow: "-12px -12px 16px 0 rgba(58, 58, 58, 0.3),12px 12px 16px 0 rgba(0,0,0,.2), -12px -12px 16px 0 rgba(58,58,58,0.1) inset, 12px 12px 16px 0 rgba(0,0,0,.2) inset"}}
                 />
             </InputGroup>
