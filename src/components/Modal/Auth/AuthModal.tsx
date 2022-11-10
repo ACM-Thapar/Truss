81442cc03b8f075c808e70c7c3fa30e02e5c8edc
Tusharbecoding <tusharchopra02122002@gmail.com>
2022-11-10 10:28:06 +0530

    added right content buttons

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -16,6 +16,8 @@ import AuthInputs from './AuthInputs';
 import OAuthButtons from './OAuthButtons';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../../firebase/clientApp';
+import ResetPassword from './ResetPassword';
+
 
 const AuthModal:React.FC = () => {
     const [modalState, setModalState] = useRecoilState(authModalState);
@@ -54,9 +56,14 @@ const AuthModal:React.FC = () => {
                         justify="center" 
                         width="70%"
                         >
-                        <OAuthButtons />
-                        <Text color="dimgray" fontWeight="700">OR</Text>
-                        <AuthInputs />
+                        {modalState.view === 'login' || modalState.view === 'signup' ? (
+                            <>
+                            <OAuthButtons />
+                            <Text color="dimgray" fontWeight="700">OR</Text>
+                            <AuthInputs />
+                            </>
+                        ) : (<ResetPassword />)}
+                        
                         </Flex>
                     </ModalBody>
                 </ModalContent>
--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -4,11 +4,11 @@ import { signOut } from 'firebase/auth';
 import { auth } from '../../../firebase/clientApp';
 import AuthButtons from './AuthButtons';
 import AuthModal from '../../Modal/Auth/AuthModal'
-
-
+import Icons from './Icons';
+import UserMenu from './UserMenu';
 
 type RightContentProps = {
-    user: any;
+    user?: User | null;
 };
 
 const RightContent:React.FC<RightContentProps> = ({ user }) => {
@@ -19,7 +19,8 @@ const RightContent:React.FC<RightContentProps> = ({ user }) => {
         <>
             <AuthModal />
             <Flex justify="center" align="center">
-                {user ? <Button onClick={() => signOut(auth)}>Signout</Button> : <AuthButtons />}
+                {user ? (<Icons />) : (<AuthButtons />)}
+                <UserMenu user={user} />
             </Flex>
         </>
     )
