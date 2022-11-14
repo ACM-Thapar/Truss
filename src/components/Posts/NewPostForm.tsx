99512dc630e9314d52a696802113a57af40a43ff
Tusharbecoding <tusharchopra02122002@gmail.com>
2022-11-14 09:59:04 +0530

    submit and newpostform

---
+++ src/components/Posts/NewPostForm.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Box, Flex, Text } from '@chakra-ui/react';
+
+type NewPostFormProps = {
+    
+};
+
+const NewPostForm:React.FC<NewPostFormProps> = () => {
+    
+    return (
+        <Flex
+        direction="column"
+        bg="white"
+        borderRadius={4}
+        mt={2}
+        >
+
+        </Flex>
+    )
+}
+export default NewPostForm;
--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
-
+import { Box, Flex, Text } from '@chakra-ui/react';
+import PageContent from '../../../components/Layout/PageContent';
+import NewPostForm from '../../../components/Posts/NewPostForm';
 
 const SubmitPostPage:React.FC = () => {
     
-    return <div>Have a good coding</div>
+    return (
+        <PageContent>
+            <>
+                <Box
+                padding="14px 0px" 
+                borderBottom="1px solid"
+                borderColor="white"
+                >
+                    <Text>
+                        Create a post
+                    </Text>
+                </Box>
+                <NewPostForm />
+            </>
+            <></>
+        </PageContent>
+    )
 }
 export default SubmitPostPage;
