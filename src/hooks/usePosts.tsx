7d85e6e9e148a261dec7e596ed1ee3ce9fd2d267
Tusharbecoding <tusharchopra02122002@gmail.com>
2022-11-25 02:13:17 +0530

    delete post

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { deleteObject ,ref } from 'firebase/storage'
+import {
+    collection,
+    deleteDoc,
+    doc,
+    getDocs,
+    onSnapshot,
+    query,
+    where,
+    writeBatch,
+} from "firebase/firestore";
 import { useRecoilState } from 'recoil';
-import { postState } from '../atoms/postsAtom'
+import { postState, Post } from '../atoms/postsAtom'
+import { storage, firestore } from '../firebase/clientApp'
 
 const usePosts:React.FC = () => {
     const [postStateValue, setPostStateValue] = useRecoilState(postState)
@@ -13,8 +25,26 @@ const usePosts:React.FC = () => {
 
     }
 
-    const onDeletePost = async () => {
+    const onDeletePost = async (post: Post): Promise<boolean> => {
+        try {
+            if (post.imageURL) {
+                const imageRef = ref(storage, `posts/${post.id}/image`)
+                await deleteObject(imageRef)
+            }
 
+            const postDocRef = doc(firestore, 'posts', post.id!)
+            await deleteDoc(postDocRef)
+
+            setPostStateValue((prev) => ({
+                ...prev,
+                posts: prev.posts.filter((item) => item.id !== post.id),
+            }))
+
+            return true;
+        } catch (error) {
+            return false;
+        }
+        
     }
 
     return {
