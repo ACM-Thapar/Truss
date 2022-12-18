10c7cdf54eaa4ee483a1438fbdfaaf2b0b8fa4c6
Tusharbecoding <tusharchopra02122002@gmail.com>
2022-12-18 11:23:16 +0530

    added about

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { GetServerSidePropsContext } from 'next';
 import { doc, runTransaction, serverTimestamp, setDoc, getDoc } from "firebase/firestore";
 import { auth, firestore } from "../../../firebase/clientApp";
-import { Community } from '../../../atoms/communitiesAtom'
+import { Community, communityState } from '../../../atoms/communitiesAtom'
 import safeJsonStringify from 'safe-json-stringify'
 import NotFound from '../../../components/Community/NotFound'
 import Header from '../../../components/Community/Header'
 import PageContent from '../../../components/Layout/PageContent'
 import CreatePostLink from '../../../components/Community/CreatePostLink'
+import About from '../../../components/Community/About'
 import Posts from '../../../components/Posts/Posts'
+import { useSetRecoilState } from 'recoil';
 
 type CommunityPageProps = {
     communityData: Community;
 };
 
 const CommunityPage:React.FC<CommunityPageProps> = ({ communityData }) => {
+    const setCommunityStateValue = useSetRecoilState(communityState)
+
+
     if (!communityData) {
         return (
             <NotFound />
         )
     }
+
+    useEffect(() => {
+        setCommunityStateValue((prev) => ({
+            ...prev,
+            currentCommunity: communityData,
+        }))
+    }, [])
+    
 
     return (
         <>
@@ -30,9 +43,7 @@ const CommunityPage:React.FC<CommunityPageProps> = ({ communityData }) => {
                     <Posts communityData={communityData} />
                 </>
                 <>
-                    <div>
-                        RHS
-                    </div>
+                    <About communityData={communityData} />
                 </>
             </PageContent>
         </>
